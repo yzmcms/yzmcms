e178b613ed90ec013f0d26c5ed34204431e6a6da
yuanzhimeng <214243830@qq.com>
2018-07-05 17:54:33 +0800

    YzmCMS初始化

--- application/admin/model/admin.class.php
+++ application/admin/model/admin.class.php
@@ -5,15 +5,15 @@ class admin {
 		$admin = D('admin');
 		$admin_login_log = D('admin_login_log');
 		$loginip = getip();
-		$address = get_address($loginip);
+		// $address = get_address($loginip);
 		$res = $admin->where(array('adminname' => $adminname))->find();
 		if(!$res){
-			$admin_login_log->insert(array('adminname'=>$adminname,'logintime'=>SYS_TIME,'loginip'=>$loginip,'address'=>$address,'password'=>$_POST['password'],'loginresult'=>'0','cause'=>L('user_does_not_exist')));
+			$admin_login_log->insert(array('adminname'=>$adminname,'logintime'=>SYS_TIME,'loginip'=>$loginip,'password'=>$_POST['password'],'loginresult'=>'0','cause'=>L('user_does_not_exist')));
 			showmsg(L('user_does_not_exist'));
 		} 
 		if($password == $res['password']){					
 			$admin->update(array('loginip'=>$loginip,'logintime'=>SYS_TIME), array('adminid'=>$res['adminid']));
-			$admin_login_log->insert(array('adminname'=>$adminname,'logintime'=>SYS_TIME,'loginip'=>$loginip,'address'=>$address,'password'=>'***','loginresult'=>'1','cause'=>L('login_success')));
+			$admin_login_log->insert(array('adminname'=>$adminname,'logintime'=>SYS_TIME,'loginip'=>$loginip,'password'=>'***','loginresult'=>'1','cause'=>L('login_success')));
 			$_SESSION['adminid'] = $res['adminid'];
 			$_SESSION['adminname'] = $res['adminname'];
 			$_SESSION['roleid'] = $res['roleid'];
@@ -22,7 +22,7 @@ class admin {
 			set_cookie('adminname', $res['adminname']);						
 			showmsg(L('login_success'), U('init'), 1);
 		}else{
-			$admin_login_log->insert(array('adminname'=>$adminname,'logintime'=>SYS_TIME,'loginip'=>$loginip,'address'=>$address,'password'=>$_POST['password'],'loginresult'=>'0','cause'=>L('password_error')));
+			$admin_login_log->insert(array('adminname'=>$adminname,'logintime'=>SYS_TIME,'loginip'=>$loginip,'password'=>$_POST['password'],'loginresult'=>'0','cause'=>L('password_error')));
 			showmsg(L('password_error'));
 		}
 	}      
