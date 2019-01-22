4a98ae9b308e96ddba3e9021dc6a52027dbc9a5f
yuanzhimeng <214243830@qq.com>
2019-01-22 16:29:24 +0800

    YzmCMS V5.2

--- application/install/database.sql
+++ application/install/database.sql
@@ -1022,8 +1022,8 @@ CREATE TABLE `yzm_module` (
   `setting` text,
   `listorder` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `disabled` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `installdate` date NOT NULL DEFAULT '0000-00-00',
-  `updatedate` date NOT NULL DEFAULT '0000-00-00',
+  `installdate` date NOT NULL DEFAULT '2016-01-01',
+  `updatedate` date NOT NULL DEFAULT '2016-01-01',
   PRIMARY KEY (`module`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
